57a3cae6a1c2fa520a1ab40d0822d20fd158d0b0
AmonRatGH <59555511+AmonRatGH@users.noreply.github.com>
2020-05-07 18:26:38 +0200

    initial release

---
+++ js/checkbox.js
@@ -0,0 +1,21 @@
+/***********************************************
+* Limit number of checked checkboxes script- by JavaScript Kit (www.javascriptkit.com)
+* This notice must stay intact for usage
+* Visit JavaScript Kit at http://www.javascriptkit.com/ for this script and 100s more
+***********************************************/
+
+function checkboxlimit(checkgroup, limit){
+	var checkgroup=checkgroup
+	var limit=limit
+	for (var i=0; i<checkgroup.length; i++){
+		checkgroup[i].onclick=function(){
+		var checkedcount=0
+		for (var i=0; i<checkgroup.length; i++)
+			checkedcount+=(checkgroup[i].checked)? 1 : 0
+		if (checkedcount>limit){
+			alert("Izbereš lahko največ "+limit+" hobije.")
+			this.checked=false
+			}
+		}
+	}
+}
